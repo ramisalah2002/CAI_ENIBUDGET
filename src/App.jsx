8e08888ab66524302626b8360ca4229bcb1c8802
Abderrahim-mn <mounouarabdrrahim2003@gmail.com>
2024-12-03 14:46:48 +0100

    rien

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "../App.css";
 
 import ChoicePage from "../pages/ChoicePage/ChoicePage";
 
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Accueil />} />
+          <Route path="/" element={<Accueil/>} />
         </Routes>
     </Router>
   );

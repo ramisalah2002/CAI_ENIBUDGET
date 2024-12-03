15b3f7f421db1b493199537b3e08b354d6c7caa0
ramisalah2002 <ramisalah2002@gmail.com>
2024-12-03 13:59:17 +0100

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
 
 import ChoicePage from "../pages/ChoicePage/ChoicePage";
 
-=======
-import Accueil from "./Accueil/Accueil.jsx"
->>>>>>> refs/remotes/origin/main
 
 function App() {
   useEffect(() => {
